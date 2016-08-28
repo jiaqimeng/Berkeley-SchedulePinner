67b8d24d34f6983cf4f549571fd7b0610cf0cf7b
ProvidenceXz <zhangxian1942@live.com>
2016-08-27 20:14:02 -0700

    Modified naming convention

--- SchedulePinner.py
+++ SchedulePinner.py
@@ -1,56 +1,63 @@
 from icalendar import Calendar, Event, Timezone, TimezoneStandard, TimezoneDaylight
 
+
 # return a .ics formatted string
 
+
 def display(cal):
-	return cal.to_ical().replace('\r\n', '\n').strip()
+    return cal.to_ical().replace('\r\n', '\n').strip()
 
-def createEvent(uid, dtstart, dtend, location, rule, summary):
-	event = Event()
-	event['uid'] = uid
-	event['dtstart'] = dtstart
-	event['dtend'] = dtend
-	event['location'] = location
-	event['summary'] = summary
-	event['rrule'] = createEventRule(rule)
-	return event
 
-def createEventRule(frequency, byday, bymonth, byyear, until, weekdaystarts):
-	return {'freq': frequency, 'byday': byday, 'bymonth': bymonth,
-	 'byyear': byyear, 'until': until, 'weekdaystarts': weekdaystarts}
+def create_event(uid, dtstart, dtend, location, rule, summary):
+    event = Event()
+    event['uid'] = uid
+    event['dtstart'] = dtstart
+    event['dtend'] = dtend
+    event['location'] = location
+    event['summary'] = summary
+    event['rrule'] = create_event_rule(rule)
+    return event
 
-def createDefaultTimezone():
-	timezone = Timezone()
-	timezone['TZID'] = 'America/Los_Angeles'
 
-	timezoneStandard = TimezoneStandard()
-	timezoneStandard['dtstart'] = '19701101T020000'
-	timezoneStandard['rrule'] = {'freq': 'yearly', 'bymonth': 11, 'byday': '1su'}
-	timezoneStandard['tzname'] = 'PST'
-	timezoneStandard['tzoffsetfrom'] = '-0700'
-	timezoneStandard['tzoffsetto'] = '-0800'
+def create_event_rule(frequency, byday, bymonth, byyear, until, weekdaystarts):
+    return {'freq': frequency, 'byday': byday, 'bymonth': bymonth,
+            'byyear': byyear, 'until': until, 'weekdaystarts': weekdaystarts}
 
-	timezoneDaylight = TimezoneDaylight()
-	timezoneDaylight['dtstart'] = '19700308T020000'
-	timezoneDaylight['rrule'] = {'freq': 'yearly', 'bymonth': 3, 'byday': '2SU'}
-	timezoneDaylight['dtstart']
-	timezoneDaylight['TZNAME'] = 'PDT'
-	timezoneDaylight['tzoffsetfrom'] = '-0800'
-	timezoneDaylight['tzoffsetto'] = '-0700'
 
-	timezone.add_component(timezoneStandard)
-	timezone.add_component(timezoneDaylight)
-	timezone['X-LIC-LOCATION'] = 'America/Los_Angeles'
-	return timezone
+def create_default_timezone():
+    timezone = Timezone()
+    timezone['TZID'] = 'America/Los_Angeles'
+
+    timezone_standard = TimezoneStandard()
+    timezone_standard['dtstart'] = '19701101T020000'
+    timezone_standard['rrule'] = {'freq': 'yearly', 'bymonth': 11, 'byday': '1su'}
+    timezone_standard['tzname'] = 'PST'
+    timezone_standard['tzoffsetfrom'] = '-0700'
+    timezone_standard['tzoffsetto'] = '-0800'
+
+    timezone_daylight = TimezoneDaylight()
+    timezone_daylight['dtstart'] = '19700308T020000'
+    timezone_daylight['rrule'] = {'freq': 'yearly', 'bymonth': 3, 'byday': '2SU'}
+    timezone_daylight['dtstart']
+    timezone_daylight['TZNAME'] = 'PDT'
+    timezone_daylight['tzoffsetfrom'] = '-0800'
+    timezone_daylight['tzoffsetto'] = '-0700'
+
+    timezone.add_component(timezone_standard)
+    timezone.add_component(timezone_daylight)
+    timezone['X-LIC-LOCATION'] = 'America/Los_Angeles'
+    return timezone
+
 
 # To do
-def addNameToCal(event, name):
-	return	
+def add_name_to_cal(event, name):
+    return
+
 
 def main():
-	cal = Calendar()
-	cal.add_component(createDefaultTimezone())
-	# TO DO: iteratively add events to calendar
-	# for ... in events:
-#		cal.add_component(createEvent(...))
+    cal = Calendar()
+    cal.add_component(create_default_timezone())
 
+# TO DO: iteratively add events to calendar
+# for ... in events:
+#       cal.add_component(createEvent(...))
